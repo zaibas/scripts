426ec75628c5602bbcd5228554a43111af0dcbdb
zaibas <zaibas@gmail.com>
2016-04-24 19:12:33 -0500

    bleh

---
+++ chocolatey/installchoco.bat
@@ -0,0 +1,8 @@
+# INSTALL CHOCOLATERY
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('http://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%systemdrive%\chocolatey\bin
+
+
+# INSTALL MARCUS DEV APPS 
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://raw.github.com/marcusoftnet/ScriptsAndStuff/master/DevMachineInstall/devApps.ps1'))" 
+
+
